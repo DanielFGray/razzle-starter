cecc462f40a402ecc201729b9f73948b8d233c2b
DanielFGray <DanielFGray@gmail.com>
2021-05-21 03:40:49 -0500

    run fixer

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import express from 'express';
+import express from 'express'
 
-let app = require('./server').default;
+let app = require('./server').default
 
 if (module.hot) {
   module.hot.accept('./server', () => {
-    console.log('🔁  HMR Reloading `./server`...');
+    console.log('🔁  HMR Reloading `./server`...')
     try {
-      app = require('./server').default;
+      app = require('./server').default
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  });
-  console.info('✅  Server-side HMR Enabled!');
+  })
+  console.info('✅  Server-side HMR Enabled!')
 }
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
 
 export default express()
   .use((req, res) => app.handle(req, res))
   .listen(port, () => {
     console.log(`> App started http://localhost:${port}`)
-  });
+  })
