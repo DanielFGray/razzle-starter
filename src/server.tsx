79a53c6827e20320a890921919410b8ed57d6b89
DanielFGray <DanielFGray@gmail.com>
2021-05-21 02:59:23 -0500

    react-helmet

--- src/server.tsx
+++ src/server.tsx
@@ -1,74 +1,76 @@
-import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { StaticRouter, StaticRouterContext } from 'react-router'
+import { HelmetProvider, FilledContext } from 'react-helmet-async'
 
-import App from './App';
+import App from './App'
 
-let assets: any;
+interface Assets {
+  [entrypoint: string]: { [asset: string]: ReadonlyArray<string> }
+}
 
-const syncLoadAssets = () => {
-  assets = require(process.env.RAZZLE_ASSETS_MANIFEST!);
-};
-syncLoadAssets();
+const assets: Assets = require(process.env.RAZZLE_ASSETS_MANIFEST!)
 
-const cssLinksFromAssets = (assets, entrypoint) => {
-  return assets[entrypoint] ? assets[entrypoint].css ?
-  assets[entrypoint].css.map(asset=>
-    `<link rel="stylesheet" href="${asset}">`
-  ).join('') : '' : '';
-};
+function cssLinksFromAssets(entrypoint: string) {
+  return assets[entrypoint]?.css
+    ?.map(asset => `<link rel="stylesheet" href="${asset}" type="text/css">`)
+    .join('') ?? ''
+}
 
-const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
-  return assets[entrypoint] ? assets[entrypoint].js ?
-  assets[entrypoint].js.map(asset=>
-    `<script src="${asset}"${extra}></script>`
-  ).join('') : '' : '';
-};
+function jsScriptTagsFromAssets(entrypoint: string, extra = '') {
+  return assets[entrypoint]?.js
+    ?.map(asset => `<script src="${asset}" type="text/javascript"${extra}></script>`)
+    .join('') ?? ''
+}
 
-export const renderApp = (req: express.Request, res: express.Response) => {
-  const context: any = {};
+export function renderApp(req: express.Request, res: express.Response) {
+  const routerCtx: StaticRouterContext = {}
+  const helmetCtx = {}
 
-  const markup = renderToString(
-    <StaticRouter context={context} location={req.url}>
-      <App />
+  const Init = (
+    <StaticRouter location={req.url} context={routerCtx}>
+      <HelmetProvider context={helmetCtx}>
+        <App />
+      </HelmetProvider>
     </StaticRouter>
-  );
+  )
 
-  if (context.url) {
-    return { redirect: context.url };
+  const markup = renderToString(Init)
+  const { helmet } = helmetCtx as FilledContext
+
+  if (routerCtx.url) return { redirect: routerCtx.url }
+
+  // prettier-ignore
+  const html = `<!doctype html>
+<html ${helmet.htmlAttributes.toString()}>
+  <head>
+    ${helmet.title.toString()}
+    ${helmet.meta.toString()}
+    ${helmet.style.toString()}
+    ${helmet.link.toString()}
+    ${cssLinksFromAssets('client')}
+  </head>
+  <body${helmet.bodyAttributes.toString()}>
+    ${helmet.noscript.toString()}
+    <div id="root">${markup}</div>
+    ${jsScriptTagsFromAssets('client', ' defer crossorigin')}
+  </body>
+</html>`
+
+  return { html }
+}
+
+const app = express()
+app.disable('x-powered-by')
+app.use(express.static(process.env.RAZZLE_PUBLIC_DIR!))
+app.get('/*', (req: express.Request, res: express.Response) => {
+  const { html = '', redirect = false } = renderApp(req, res)
+  if (redirect) {
+    res.redirect(redirect)
   } else {
-    const html =
-      // prettier-ignore
-      `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charSet='utf-8' />
-        <title>Welcome to Razzle</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${cssLinksFromAssets(assets, 'client')}
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-        ${jsScriptTagsFromAssets(assets, 'client', ' defer crossorigin')}
-    </body>
-  </html>`;
-
-    return { html };
+    res.send(html)
   }
-};
+})
 
-const server = express()
-  .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR!))
-  .get('/*', (req: express.Request, res: express.Response) => {
-    const { html = '', redirect = false } = renderApp(req, res);
-    if (redirect) {
-      res.redirect(redirect);
-    } else {
-      res.send(html);
-    }
-  });
-
-export default server;
+export default app
